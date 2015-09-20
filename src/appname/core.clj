313e0bf3a4e51fd2d4da97907fad173af1171c9e
Yen-Chin Lee <coldnew.tw@gmail.com>
2015-09-21 00:36:31 +0800

    first commit
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

---
+++ src/appname/core.clj
@@ -0,0 +1,6 @@
+(ns appname.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/appname/core_test.clj
@@ -0,0 +1,7 @@
+(ns appname.core-test
+  (:require [clojure.test :refer :all]
+            [appname.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
